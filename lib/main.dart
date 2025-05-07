bfb312b4275a39f412d0b703892ead914dfcf426
Nihal-Mohammed <nihalmhdv25@gmail.com>
2025-05-07 15:23:07 +0530

    Hive -category database added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:money_management_app/models/category/category_model.dart';
 import 'package:money_management_app/screens/home/home_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
+  if (!Hive.isAdapterRegistered(CategoryTypeAdapter().typeId)) {
+    Hive.registerAdapter(CategoryTypeAdapter());
+  }
   runApp(const MainApp());
 }
 
